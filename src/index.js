6cd32ff466e2af51d2521061664928ab21acacce
pyrschool <72523829+pyrschool@users.noreply.github.com>
2021-01-21 01:02:43 +0200

    Two HTML elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
-
-let h1 = document.createElement("h1");
-h1.innerHTML = "Hello World!";
-document.getElementById("root").appendChild(h1);
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>React at work</p>
+  </div>,
+  document.getElementById("root")
+);
