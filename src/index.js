3a7624c3956a48ca4be184609cec65d65adf5654
pyrschool <72523829+pyrschool@users.noreply.github.com>
2021-01-23 15:41:07 +0200

    JSX expressions - simple variable

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
+let name = "Dear Dev";
+let message = "First do it; then do it right; then do it better.";
+
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>React at work</p>
+    <h1>Hello {name}!</h1>
+    <p>React at work says: {message}</p>
   </div>,
   document.getElementById("root")
 );
