d6507b3090597651553dc97507035ab579fb0a1e
pyrschool <72523829+pyrschool@users.noreply.github.com>
2021-01-24 22:40:16 +0200

    JSX expressions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ var ReactDOM = require("react-dom");
 let name = "Dear Dev";
 let message = "First do it; then do it right; then do it better.";
 
+let nameMessage;
+if (name.length < 6) nameMessage = "short";
+else nameMessage = "long";
+
+let today = new Date();
+
 ReactDOM.render(
   <div>
     <h1>Hello {name}!</h1>
     <p>React at work says: {message}</p>
+    <p>
+      Your name is {name.length < 6 ? "short" : "long"}. (ternary operator
+      expression)
+    </p>
+    <p>Your name is {nameMessage}. (variable and if-statement)</p>
+    <p>Today we have {today.toString()}</p>
   </div>,
   document.getElementById("root")
 );
